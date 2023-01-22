87a9bdfa0046461c8b31a71d51fd4e2c039e5cf1
Ben Chan <benchan79@gmail.com>
2023-01-22 15:05:22 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [cityName, setCityName] = useState("");
+  const [weather, setWeather] = useState(null);
+
+  const getURL = async () => {
+    const url = `/.netlify/functions/owm?cityName=${cityName}`;
+    try {
+      const response = await fetch(url).then((res) => res.json())
+      // console.log("App: response");
+      console.log(response);
+      setWeather(response[0])
+    } catch (err) {
+      console.log("App: err");
+      console.log(err);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setCityName(e.target.value);
+  };
+
+  const handleOnClick = (e) => {
+    getURL();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        onChange={handleInputChange}
+        name="city"
+        type="text"
+        placeholder="city"
+        value={cityName}
+      />
+
+      <button onClick={handleOnClick}>Click</button>
+
+      <h1>{cityName}</h1>
+      {weather && (
+        <>
+          <h1>
+            {weather.name}, {weather.country}
+          </h1>
+          <h2>
+            {weather.lat}, {weather.lon}
+          </h2>
+        </>
+      )}
     </div>
   );
 }
